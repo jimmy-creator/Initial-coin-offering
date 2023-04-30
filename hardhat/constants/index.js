f61bb9f70bbf1dd89c0d4b35f242a533edd94391
Muhammed Jasim <muhammedjasim@Muhammeds-MacBook-Air.local>
2023-04-30 17:30:37 +0530

    eod in april

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x8593DD17A8D2Bab736b26584b82C79c44f6a2c63";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
